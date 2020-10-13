d0f3d1816b237c5f5423e5c504cb5dcbb5ed9d36
uchikun2493 <um2493@gmail.com>
2020-10-14 00:48:38 +0900

    update sample 02: add bin

--- samples/02_color_convert.py
+++ samples/02_color_convert.py
@@ -3,9 +3,20 @@ import cv2
 
 # main
 def main():
-    img = cv2.imread('../data/cat.png')
+    # sample image read 
+    img = cv2.imread('../data/nabekuro.png')
+
+    # RGB to grayscale
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('../output/gray.png', img_gray)
+    # save grayscale image
+    cv2.imwrite('../output/02/gray.png', img_gray)
+
+    # Binarization
+    # Automatically determine the threshold
+    threshold, img_bin = cv2.threshold(
+        img_gray, 0, 255, cv2.THRESH_OTSU)
+    # save bin. image
+    cv2.imwrite('../output/02/bin.png', img_bin)
 
 if __name__ == '__main__':
     main()
